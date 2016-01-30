04ce31c794b71c4e9129786a933cc8bf49ddef28
Malte Zacharias <mz-basis+git@asenwelt.de>
2016-01-30 03:36:05 +0100

    First running test version with reliable message transport Problems occured when ... ... setImmediate was used vs. setTimeout in sender ... multiple replys were sent in one receiver block (duh) ... logging doesn't seem to improve or worsen performance, 10k msgs were sent in about 11 secs, but 5 sender threads had the same performance which is about 800 msg / sec. not that fast debugging is quite hard :(

---
+++ Procfile
@@ -0,0 +1,1 @@
+db-updater: node db-updater.js | bunyan
--- db-updater.js
+++ db-updater.js
@@ -1,2 +1,38 @@
 var bunyan = require('bunyan');
-var log = bunyan.createLogger({name: 'myapp'});
+var log = bunyan.createLogger({name: 'kga-web/db-updater'});
+var endpoint = process.env.ENDPOINTS_DBUPDATER_SOCKET;
+var maximum_message_size = process.env.ENDPOINTS_DBUPDATER_MAXMESSAGESIZE;
+var zmq = require('zmq');
+log.info('ZMQ', zmq.version);
+log.info('Starting db-updater configured to provide endpoint',endpoint);
+log.info('Maximum message size', maximum_message_size);
+var responder = zmq.socket('rep', { ZMQ_MAXMSGSIZE: maximum_message_size });
+responder.on('message', function(request) {
+  try {
+    var messageData = JSON.parse(request.toString());
+    log.info("Message received", messageData);
+    //responder.send("{}");
+    responder.send(JSON.stringify({text: 'World', counter: messageData.counter }));
+  } catch (syntaxError){
+    log.error("Parsing of message failed", { message: request.toString(), error: syntaxError });
+    responder.send(JSON.stringify({
+      status: "Error",
+      message: syntaxError.toString()
+    }));
+  }
+});
+
+responder.bind(endpoint, function(err){
+  if (err) {
+    log.error("Failed initializing message receiver", { error: err });
+    process.exit(1);
+  } else {
+    log.info("Listening on", endpoint);
+  }
+});
+
+process.on('SIGINT', function() {
+  log.info('Received SIGINT, closing socket', endpoint);
+  responder.close();
+  process.exit(0);
+})
